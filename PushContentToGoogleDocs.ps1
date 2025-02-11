fa0665218a916e1959f626376e6291b22f6ca19d
AndyDavo <andy@wearenova.co.uk>
2025-02-11 11:47:54 +0000

    fix for file extensions

--- PushContentToGoogleDocs.ps1
+++ PushContentToGoogleDocs.ps1
@@ -6,8 +6,8 @@ $SourceFile1 = "C:\localhost\recon1\index.html"
 $SourceFile2 = "C:\localhost\recon1\InvestorDeck.html"
 
 # Destination files (replace with your desired destination paths)
-$DestFile1 = "G:\Shared drives\Recon1\Recon1 - Website.html"
-$DestFile2 = "G:\Shared drives\Recon1\Recon1 - Investor Deck.html"
+$DestFile1 = "G:\Shared drives\Recon1\Recon1 - Website"
+$DestFile2 = "G:\Shared drives\Recon1\Recon1 - Investor Deck"
 
 # Perform the file copies
 try {
